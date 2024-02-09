e6d1afe5f2f5cc106a850efb765aacd2b2455772
Jonatandb <jonatandb@gmail.com>
2024-02-09 14:05:26 -0300

    Do-while and Foreach practice

---
+++ DoWhileAndForEach/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+
+foreach (string innerString in myStrings) {
+
+  string myString = innerString;
+
+  int periodLocation = 0;
+
+  do {
+
+    periodLocation = myString.IndexOf(".");
+
+    if (periodLocation != -1) {
+
+        Console.WriteLine(myString.Substring(0, periodLocation).TrimStart());
+        myString = myString.Remove(0, periodLocation + 1);
+
+    } else if (myString.Length > 0) {
+
+        Console.WriteLine(myString.TrimStart());
+
+    }
+
+  } while(periodLocation > 0);
+
+}
+
+
+Console.ReadLine();
