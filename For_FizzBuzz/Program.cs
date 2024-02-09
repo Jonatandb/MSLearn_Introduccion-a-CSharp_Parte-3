5a875805d51335eb023930e6cd5b7451ffb3bc0d
Jonatandb <jonatandb@gmail.com>
2024-02-09 12:00:17 -0300

    For, If practice

---
+++ For_FizzBuzz/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+for(int n = 1; n <= 100; n++) {
+  if ((n % 3 == 0) && (n % 5 == 0)) {
+    Console.WriteLine($"{n} - FizzBuzz");
+  } else if (n % 3 == 0) {
+    Console.WriteLine($"{n} - Fizz");
+  } else if (n % 5 == 0) {
+    Console.WriteLine($"{n} - Buzz");
+  } else {
+    Console.WriteLine($"{n}");
+  }
+}
+
+Console.ReadLine();
