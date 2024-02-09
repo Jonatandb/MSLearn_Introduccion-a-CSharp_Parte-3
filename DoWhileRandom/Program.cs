08ca70948f147774e507b1baeca240cf7e78384c
Jonatandb <jonatandb@gmail.com>
2024-02-09 12:00:50 -0300

    Do-while, Random practice

---
+++ DoWhileRandom/Program.cs
@@ -0,0 +1,12 @@
+﻿Random random = new Random();
+
+Console.Clear();
+
+int current = 0;
+
+do {
+  current = random.Next(1,11);
+  Console.WriteLine($"Current: {current}");
+} while (current != 7);
+
+Console.ReadLine();
