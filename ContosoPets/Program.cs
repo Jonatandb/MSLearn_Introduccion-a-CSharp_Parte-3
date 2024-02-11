fab8da235fca35d723a6bfefa1a9d4f3d8746921
Jonatandb <jonatandb@gmail.com>
2024-02-11 13:33:26 -0300

    Use of If and Do-While for ask user input and validation

--- ContosoPets/Program.cs
+++ ContosoPets/Program.cs
@@ -266,7 +266,37 @@ do {
 
 		case "4":
 			// Ensure animal nicknames and personality descriptions are complete"
-			Console.WriteLine("Challenge Project - please check back soon to see progress.");
+			for (int i = 0; i < maxPets; i++) {
+				string currentID = ourAnimals[i, 0];
+				if (currentID != "ID #: ") {
+
+					if (ourAnimals[i, 3] == "Nickname: " || ourAnimals[i, 3] == "Nickname: tbd") {
+						// Ask for the pet's nickname.
+						do {
+							Console.WriteLine($"Enter a nickname for {currentID}");
+							readResult = Console.ReadLine();
+							if (readResult != null) {
+								animalNickname = readResult.Trim().ToLower();
+							}
+						} while (string.IsNullOrEmpty(animalNickname) == true);
+						ourAnimals[i, 3] = "Nickname: " + animalNickname;
+					}
+
+					if (ourAnimals[i, 5] == "Personality: tbd" || ourAnimals[i, 5] == "Personality: ") {
+						// Ask for a description of the pet's personality.
+						do {
+							Console.WriteLine($"Enter a personality description for {currentID} (likes or dislikes, tricks, energy level)");
+							readResult = Console.ReadLine();
+							if (readResult != null) {
+								animalPersonalityDescription = readResult.Trim().ToLower();
+							}
+						} while (string.IsNullOrEmpty(animalPersonalityDescription) == true);
+						ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
+					}
+
+				}
+			}
+			Console.WriteLine("Nickname and personality description fields are complete for all of our friends."); Console.WriteLine("Press the Enter key to continue.");
 			Console.WriteLine("Press the Enter key to continue.");
 			readResult = Console.ReadLine();
 			break;
